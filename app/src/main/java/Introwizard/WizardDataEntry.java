4598f932658b802cc7167ca592ff84ef367b9531
Fredrik Ståhl <fredrik.stahl@softhouse.se>
2016-02-26 18:11:13 +0100

    add new activity wizard data entry for entering data on first app run

---
+++ app/src/main/java/Introwizard/WizardDataEntry.java
@@ -0,0 +1,20 @@
+package Introwizard;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.myfitnessjourney.Controller.R;
+
+/**
+ * Created by fst on 2016-02-26.
+ */
+public class WizardDataEntry extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wizard_vp_layout);
+
+    }
+}
